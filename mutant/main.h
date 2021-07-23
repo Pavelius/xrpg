386b6c04bca1fe8374f7fbd0492fe25b9aaa88c5
Pavelius <p000000000001@gmail.com>
2021-07-23 07:50:51 +0300

    Туман войны

--- mutant/main.h
+++ mutant/main.h
@@ -13,3 +13,10 @@ enum attribute_s : unsigned char {
 enum skill_s : unsigned char {
 	Endure, Force, Fight, Sneak, Move, Shoot, Scout, Comprehend, KnowTheZone, SenceEmotion, Manipulate, Heal, Intimidate,
 };
+template<int count>
+struct datamap {
+	constexpr static unsigned perbyte = 4;
+	unsigned char		data[(count + perbyte - 1) / perbyte];
+	int					get(short unsigned i) const { return (data[i / perbyte] >> ((i % perbyte) * 2)) & 0x03; }
+	void				set(short unsigned i, int v) { data[i / perbyte] |= (v & 0x03) << ((i % perbyte) * 2); }
+};
