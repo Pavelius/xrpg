8d8012ae36574aa4bd1af1103b280f2c2c69a291
ws-1113 <p000000000001@gmail.com>
2021-07-27 15:27:27 +0300

    Подготовка функционала по переводу

--- xrpg/translate.cpp
+++ xrpg/translate.cpp
@@ -1,12 +1,14 @@
 #include "crt.h"
 
-namespace {
-struct translatei {
+struct translate {
 	const char*			id;
 	const char*			name;
+	static void			initialize(const char* lid);
 };
-}
-template<> array bsdata<translatei>::source(sizeof(translatei));
+template<> array bsdata<translate>::source(sizeof(translate));
+static array source_text(sizeof(translate));
+
+bool readl(const char* url, array& source, unsigned* fields, int fields_count, unsigned* special, int special_count);
 
 static int compare(const void* v1, const void* v2) {
 	auto p1 = (const translatei*)v1;
@@ -14,27 +16,29 @@ static int compare(const void* v1, const void* v2) {
 	return strcmp(p1->id, p2->id);
 }
 
-static void update_elements() {
-	auto& ei = bsdata<translatei>::source;
+static void update_elements(array& ei) {
 	if(ei.getcount()==0)
 		return;
 	qsort(ei.data, ei.getcount(), ei.getsize(), compare);
 }
 
-void inittranslate() {
+void translate::initialize(const char* lid) {
 	auto p = bsdata<translatei>::add();
 	p->id = "copy";
-	p->name = "Копировать";
+	p->name = "Copy";
 	p = bsdata<translatei>::add();
 	p->id = "cut";
-	p->name = "Вырезать";
-	update_elements();
+	p->name = "Cut";
+	update_elements(bsdata<translatei>::source);
+	update_elements(source_text);
 }
 
 const char* szt(const char* id) {
+	if(!id || id[0]==0)
+		return "";
 	translatei key = {id, 0};
 	auto p = (translatei*)bsearch(&key, bsdata<translatei>::source.data, bsdata<translatei>::source.getcount(), bsdata<translatei>::source.getsize(), compare);
 	if(!p || !p->name)
-		return "";
+		return id;
 	return p->name;
 }
